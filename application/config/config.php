7724849819ec405eee8532019ddb8dfaef628c56
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-01-01 18:10:36 +0530

    initialization

---
+++ application/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('BASE_URL', 'http://localhost/arunkumar-mvc/');
+define('PUBLIC_URL', BASE_URL . 'public/');
+define('XML_SRC_URL', BASE_URL . 'md-src/xml/');
+define('GALLERY_URL', PUBLIC_URL . 'images/gallery/');
+
+// Physical location of resources
+define('PHY_BASE_URL', '/var/www/html/arunkumar-mvc/');
+define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
+define('PHY_XML_SRC_URL', PHY_BASE_URL . 'md-src/xml/');
+define('PHY_FLAT_URL', PHY_BASE_URL . 'application/views/flat/');
+define('PHY_GALLERY_URL', PHY_PUBLIC_URL . 'images/gallery/');
+
+define('DB_PREFIX', 'arun');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'arunkumar');
+define('arunkumar_USER', 'root');
+define('arunkumar_PASSWORD', 'mysql');
+
+?>
